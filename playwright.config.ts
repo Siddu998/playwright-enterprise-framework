b253ef3a194dc12794b49ad614579c21021f0e9c
nagaraju-dasi <105856992+Siddu998@users.noreply.github.com>
2026-05-22 14:29:47 +0530

    Create playwright.config.ts
    Add Playwright config

---
+++ playwright.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+
+  timeout: 30000,
+
+  use: {
+    browserName: 'chromium',
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on-first-retry'
+  },
+
+  reporter: [
+    ['html'],
+    ['list']
+  ]
+});
