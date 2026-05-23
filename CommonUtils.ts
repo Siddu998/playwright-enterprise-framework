0d2b1e1222b32db2c3db358d499ba398bd115924
nagaraju-dasi <105856992+Siddu998@users.noreply.github.com>
2026-05-23 11:33:46 +0530

    Update CommonUtils.ts
    Add reusable validation utility

--- CommonUtils.ts
+++ CommonUtils.ts
@@ -1,10 +1,49 @@
 export class CommonUtils {
 
-  static generateRandomEmail() {
+  static generateRandomEmail(): string {
+
     return `testuser${Date.now()}@mail.com`;
+
   }
 
-  static getCurrentDate() {
-    return new Date().toISOString();
+  static generateRandomNumber(): number {
+
+    return Math.floor(Math.random() * 100000);
+
   }
+
+  static getCurrentDate(): string {
+
+    return new Date().toISOString();
+
+  }
+
+  static validateText(
+    actual: string,
+    expected: string
+  ): void {
+
+    if (actual !== expected) {
+
+      throw new Error(
+        `
+        Validation Failed
+        Actual Value   : ${actual}
+        Expected Value : ${expected}
+        `
+      );
+
+    }
+
+  }
+
+  static formatLogMessage(
+    module: string,
+    message: string
+  ): string {
+
+    return `[${module}] ${message}`;
+
+  }
+
 }
