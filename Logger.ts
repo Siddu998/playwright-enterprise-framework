068a1be91b2d93ec7dd84b0098ced3c2a81a0c1b
nagaraju-dasi <105856992+Siddu998@users.noreply.github.com>
2026-05-23 11:23:26 +0530

    Update Logger.ts
    Enhance framework logger utility

--- Logger.ts
+++ Logger.ts
@@ -1,14 +1,27 @@
 export class Logger {
 
   static info(message: string) {
-    console.log(`[INFO] ${new Date().toISOString()} - ${message}`);
+    console.log(
+      `[INFO] [${new Date().toISOString()}] ${message}`
+    );
   }
 
   static error(message: string) {
-    console.error(`[ERROR] ${new Date().toISOString()} - ${message}`);
+    console.error(
+      `[ERROR] [${new Date().toISOString()}] ${message}`
+    );
   }
 
   static warn(message: string) {
-    console.warn(`[WARN] ${new Date().toISOString()} - ${message}`);
+    console.warn(
+      `[WARN] [${new Date().toISOString()}] ${message}`
+    );
   }
+
+  static success(message: string) {
+    console.log(
+      `[SUCCESS] [${new Date().toISOString()}] ${message}`
+    );
+  }
+
 }
