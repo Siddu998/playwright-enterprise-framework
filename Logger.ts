16b12295307c9468d334cdcef660661ca930143b
nagaraju-dasi <105856992+Siddu998@users.noreply.github.com>
2026-05-23 11:36:00 +0530

    Update Logger.ts

--- Logger.ts
+++ Logger.ts
@@ -1,27 +1,35 @@
 export class Logger {
 
-  static info(message: string) {
+  static info(message: string): void {
+
     console.log(
       `[INFO] [${new Date().toISOString()}] ${message}`
     );
+
   }
 
-  static error(message: string) {
-    console.error(
-      `[ERROR] [${new Date().toISOString()}] ${message}`
-    );
-  }
+  static success(message: string): void {
 
-  static warn(message: string) {
-    console.warn(
-      `[WARN] [${new Date().toISOString()}] ${message}`
-    );
-  }
-
-  static success(message: string) {
     console.log(
       `[SUCCESS] [${new Date().toISOString()}] ${message}`
     );
+
+  }
+
+  static warn(message: string): void {
+
+    console.warn(
+      `[WARN] [${new Date().toISOString()}] ${message}`
+    );
+
+  }
+
+  static error(message: string): void {
+
+    console.error(
+      `[ERROR] [${new Date().toISOString()}] ${message}`
+    );
+
   }
 
 }
