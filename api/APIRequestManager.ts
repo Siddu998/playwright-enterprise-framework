a439ce5388119a3bc2bd060b65a90e4f6fe86a40
nagaraju-dasi <105856992+Siddu998@users.noreply.github.com>
2026-05-23 12:06:26 +0530

    Create APIRequestManager.ts

---
+++ api/APIRequestManager.ts
@@ -0,0 +1,81 @@
+import {
+  request,
+  APIRequestContext,
+  APIResponse
+} from '@playwright/test';
+
+import { Logger } from '../utils/Logger';
+
+export class APIRequestManager {
+
+  private apiContext!: APIRequestContext;
+
+  async initialize(): Promise<void> {
+
+    Logger.info('Initializing API Context');
+
+    this.apiContext = await request.newContext({
+
+      baseURL: 'https://reqres.in',
+
+      extraHTTPHeaders: {
+
+        'Content-Type': 'application/json'
+
+      }
+
+    });
+
+  }
+
+  async get(
+    endpoint: string
+  ): Promise<APIResponse> {
+
+    Logger.info(`GET Request: ${endpoint}`);
+
+    return await this.apiContext.get(endpoint);
+
+  }
+
+  async post(
+    endpoint: string,
+    payload: object
+  ): Promise<APIResponse> {
+
+    Logger.info(`POST Request: ${endpoint}`);
+
+    return await this.apiContext.post(endpoint, {
+
+      data: payload
+
+    });
+
+  }
+
+  async put(
+    endpoint: string,
+    payload: object
+  ): Promise<APIResponse> {
+
+    Logger.info(`PUT Request: ${endpoint}`);
+
+    return await this.apiContext.put(endpoint, {
+
+      data: payload
+
+    });
+
+  }
+
+  async delete(
+    endpoint: string
+  ): Promise<APIResponse> {
+
+    Logger.info(`DELETE Request: ${endpoint}`);
+
+    return await this.apiContext.delete(endpoint);
+
+  }
+
+}
