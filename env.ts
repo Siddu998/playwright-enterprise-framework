e132777ede7658d20e6ffb902a84bf482f90c1b4
nagaraju-dasi <105856992+Siddu998@users.noreply.github.com>
2026-05-23 11:42:44 +0530

    Update env.ts

--- env.ts
+++ env.ts
@@ -1,11 +1,23 @@
 export const ENV = {
+
   qa: {
+
     baseURL: 'https://qa.example.com'
+
   },
+
   staging: {
+
     baseURL: 'https://staging.example.com'
+
   },
+
   production: {
+
     baseURL: 'https://example.com'
+
   }
+
 };
+
+export const currentEnv = ENV.qa;
